bcf85aec3863aef7298f548abe024842b4b9f279
Inwoo Jeong <sinth92@hotmail.com>
2021-11-08 11:40:50 +0900

    Firebase SDK 합체 중

--- app/src/main/java/com/cookandroid/udonpro/MainActivity.kt
+++ app/src/main/java/com/cookandroid/udonpro/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.cookandroid.udonpro
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
