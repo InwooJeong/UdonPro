1800bc38f005d3c45f257e763ce4f1a203e0bbee
Inwoo Jeong <sinth92@hotmail.com>
2021-11-15 11:17:15 +0900

    delete reported member

--- app/src/main/java/com/cookandroid/udonpro/MainActivity.kt
+++ app/src/main/java/com/cookandroid/udonpro/MainActivity.kt
@@ -2,41 +2,62 @@ package com.cookandroid.udonpro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.member_list.*
-import kotlinx.android.synthetic.main.mlist_item.*
+import android.view.MenuItem
+import com.google.android.material.navigation.NavigationBarView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    lateinit var mAdapter : MemberListAdapter
-    val viewModel by lazy { ViewModelProvider(this).get(MemberViewModel::class.java) }
+
+class MainActivity : AppCompatActivity(),
+    NavigationBarView.OnItemSelectedListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.member_list)
+        setContentView(R.layout.activity_main)
+        bottom_navigation.setOnItemSelectedListener(this)
 
-        mAdapter = MemberListAdapter(this)
 
-        val recyclerView : RecyclerView = findViewById(R.id.memberListView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = mAdapter
-        observerData()
+//        chatButton.setOnClickListener() {
+//            val intent = Intent(applicationContext, ChatRoomActivity::class.java)
+//            startActivity(intent)
+//        }
+
+//        val writeBtn = findViewById<Button>(R.id.writeBtn)
+//
+//        writeBtn.setOnClickListener {
+//            val database = Firebase.database
+//
+//            val myRef = database.getReference("message")
+//
+//            myRef.setValue("Hello, World!")
     }
 
-    fun observerData(){
-        viewModel.fetchData().observe(this, Observer {
-            mAdapter.setListData(it)
-            mAdapter.notifyDataSetChanged()
-        })
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.btn_home ->{
+                var page1 = MainForm()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, page1).commit()
+                return true
+            }
+            R.id.btn_favorites ->{
+                var page2 = MainForm_Request()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, page2).commit()
+                return true
+            }
+            R.id.btn_registerBook ->{
+                var page3 = RegisterBook()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, page3).commit()
+                return true
+            }
+            R.id.btn_mypage ->{
+                var page4 = newMypage()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, page4).commit()
+                return true
+            }
+        }
+        return false
     }
+
+
 }
