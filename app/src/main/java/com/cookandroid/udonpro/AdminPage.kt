75098de92cd55585b23b8538e7ce347e9d075c50
Inwoo Jeong <sinth92@hotmail.com>
2021-11-16 14:00:49 +0900

    Connect Page

--- app/src/main/java/com/cookandroid/udonpro/AdminPage.kt
+++ app/src/main/java/com/cookandroid/udonpro/AdminPage.kt
@@ -1,7 +1,29 @@
 package com.cookandroid.udonpro
 
-import androidx.fragment.app.Fragment
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class AdminPage : Fragment() {
+class AdminPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.admin_page)
+
+        val btn_bookList = findViewById<Button>(R.id.btn_bookList)
+        val button5 = findViewById<Button>(R.id.button5)
+
+        btn_bookList.setOnClickListener{
+            val intent = Intent(this, ReportedList::class.java)
+            startActivity(intent)
+        }
+
+        button5.setOnClickListener{
+            val intent = Intent(this, MemberList::class.java)
+            startActivity(intent)
+        }
+
+    }
 
 }
